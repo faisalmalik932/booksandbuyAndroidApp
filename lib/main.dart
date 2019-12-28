9e6d62c00912dcc6f0576ef92e6a766c394a5bb2
Faisal Malik <faisallmalick@gmail.com>
2019-12-27 18:30:56 -0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+void main() => runApp(MaterialApp(
+  home: MyApp(),
+));
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+
+}
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return WebviewScaffold(
+      appBar: AppBar(
+      ),
+      url: "https://www.booksandbuy.com/",
+      withZoom: true,
+    );
+  }
+}
